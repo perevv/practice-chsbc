014e4d55d19875932ac0d7dc3db6dd5ad90d2c2a
perevv <36760704+perevv@users.noreply.github.com>
2018-06-20 19:54:57 +0300

    Add files via upload

---
+++ 2A11.py
@@ -0,0 +1,29 @@
+import winsound
+
+ls = ((659,250),(659,250))
+
+
+
+	
+winsound.Beep(659,250)
+winsound.Beep(659,250)
+winsound.Beep(659,300)
+winsound.Beep(523,250)
+winsound.Beep(659,250)
+winsound.Beep(784,300)
+winsound.Beep(392,300)
+winsound.Beep(523,275)
+winsound.Beep(392,275)
+winsound.Beep(330,275)
+winsound.Beep(440,250)
+winsound.Beep(494,250)
+winsound.Beep(466,275)
+winsound.Beep(392,275)
+winsound.Beep(659,250)
+winsound.Beep(784,250)
+winsound.Beep(880,275)
+winsound.Beep(784,225)
+winsound.Beep(659,250)
+winsound.Beep(523,250)
+winsound.Beep(587,225)
+winsound.Beep(494,225)
